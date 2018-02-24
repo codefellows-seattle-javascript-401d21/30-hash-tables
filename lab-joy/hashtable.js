e3b590f50b84495ee6beb62c94987c1f4c9131fe
strawbee <yuejiao.hou@gmail.com>
2018-02-24 15:47:49 -0800

    completed pending tests

---
+++ lab-joy/hashtable.js
@@ -0,0 +1,45 @@
+'use strict';
+
+let SLL = require('./lib/sll');
+
+const HashTable = module.exports = function(size) {
+  this.size = size || 1024;
+  this.buckets = [...Array(this.size)]; 
+};
+
+HashTable.prototype.hash = function(key) {
+  let total = 0;
+  for (let i in key) total += key.charCodeAt(i);
+  return total % this.buckets.length;
+};
+
+HashTable.prototype.set = function(key, value) {
+  let index = this.hash(key);
+  if (!this.buckets[index]) this.buckets[index] = (new SLL).insertTail(key, value);
+  else this.buckets[index].insertTail(key, value);
+};
+
+HashTable.prototype.get = function(key) {
+  let bucket = this.buckets[this.hash(key)];
+  if (!bucket) return null;
+  let current = bucket.head;
+  while (current) {
+    if (current.key === key) return current.value;
+    current = current.next;
+  }
+  return null;
+};
+
+HashTable.prototype.remove = function(key) {
+  let bucket = this.buckets[this.hash(key)];
+  if (!bucket) return null;
+  let current = bucket.head;
+  if (!current.next) delete bucket.head;
+  while (current.next) {
+    let prev = current;
+    current = current.next;
+    if (current.key === key) prev.next = current.next;
+  }
+};
+
+module.exports = HashTable;
---
+++ lab-joy/test/hashtable.js
@@ -0,0 +1,5 @@
+'use strict';
+
+require('jest');
+let HT = require('../hashtable');
+
