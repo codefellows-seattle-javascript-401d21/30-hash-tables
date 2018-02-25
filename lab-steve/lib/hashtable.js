3c12f575d5d96d4b64d57196d96872b6f947720c
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-25 15:04:16 -0800

    Implemented constructor and skel structure for hashtable

---
+++ lab-steve/lib/hashtable.js
@@ -0,0 +1,31 @@
+'use strict';
+
+class Pair {
+  constructor(key, value) {
+    if (!key || typeof key !== 'string') throw new Error(`invalid key ${key}`);
+
+    this.key = key;
+    this.value = value;
+  }
+}
+
+// Hashtable implementation
+module.exports = class {
+  constructor(size = 1024) {
+    if (!size || typeof size !== 'number')
+      throw new Error(`invalid size ${size}`);
+    if (size <= 0) throw new Error(`invalid size value ${size}`);
+
+    // Allocate storage
+    this.bucket = Array(size).fill(null);
+    this.size = size;
+  }
+
+  _hash(key) {}
+
+  get(key) {}
+
+  set(key, value) {}
+
+  remove(key) {}
+};
