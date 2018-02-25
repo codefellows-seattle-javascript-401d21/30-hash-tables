8e0637d8a0cffd6ea3aafc9125beb6bc3136a824
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-25 15:37:01 -0800

    Tests for the constructor written

--- lab-steve/__test__/hashtable.test.js
+++ lab-steve/__test__/hashtable.test.js
@@ -6,14 +6,30 @@ const HashTable = require('../lib/hashtable');
 describe('HashTable', () => {
   describe('#constructor', () => {
     describe('Valid', () => {
-      it('should be truthy', () => {
-        expect(true).toBeTruthy();
+      it('should return back a valid HashTable object', () => {
+        expect(new HashTable()).toBeInstanceOf(HashTable);
+      });
+
+      it('should have a default size of 1024 if no size was provided', () => {
+        let ht = new HashTable();
+        expect(ht.size).toEqual(1024);
+      });
+
+      it('should allocate "size" buckets to store data in', () => {
+        let ht = new HashTable();
+        expect(ht.bucket.length).toEqual(1024);
+        expect(ht.bucket).toBeInstanceOf(Array);
       });
     });
 
     describe('Invalid', () => {
-      it('should be truthy', () => {
-        expect(true).toBeTruthy();
+      it('should throw an Error if the requested size is < 0', () => {
+        expect(() => new HashTable(-10)).toThrow('invalid size value -10');
+      });
+
+      it('should throw an Error if the size arg is a non-number', () => {
+        expect(() => new HashTable({})).toThrow('invalid size [object Object]');
+        expect(() => new HashTable('foo')).toThrow('invalid size foo');
       });
     });
   });
