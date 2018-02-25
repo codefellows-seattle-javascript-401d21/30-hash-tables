27a007eafb976c113984fc172a2ccccb1b0a1a40
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-25 15:03:58 -0800

    Scaffolded hashtable test setup

---
+++ lab-steve/__test__/hashtable.test.js
@@ -0,0 +1,76 @@
+'use strict';
+
+require('jest');
+const HashTable = require('../lib/hashtable');
+
+describe('HashTable', () => {
+  describe('#constructor', () => {
+    describe('Valid', () => {
+      it('should be truthy', () => {
+        expect(true).toBeTruthy();
+      });
+    });
+
+    describe('Invalid', () => {
+      it('should be truthy', () => {
+        expect(true).toBeTruthy();
+      });
+    });
+  });
+
+  describe('#_hash', () => {
+    describe('Valid', () => {
+      it('should be truthy', () => {
+        expect(true).toBeTruthy();
+      });
+    });
+
+    describe('Invalid', () => {
+      it('should be truthy', () => {
+        expect(true).toBeTruthy();
+      });
+    });
+  });
+
+  describe('#get', () => {
+    describe('Valid', () => {
+      it('should be truthy', () => {
+        expect(true).toBeTruthy();
+      });
+    });
+
+    describe('Invalid', () => {
+      it('should be truthy', () => {
+        expect(true).toBeTruthy();
+      });
+    });
+  });
+
+  describe('#set', () => {
+    describe('Valid', () => {
+      it('should be truthy', () => {
+        expect(true).toBeTruthy();
+      });
+    });
+
+    describe('Invalid', () => {
+      it('should be truthy', () => {
+        expect(true).toBeTruthy();
+      });
+    });
+  });
+
+  describe('#remove', () => {
+    describe('Valid', () => {
+      it('should be truthy', () => {
+        expect(true).toBeTruthy();
+      });
+    });
+
+    describe('Invalid', () => {
+      it('should be truthy', () => {
+        expect(true).toBeTruthy();
+      });
+    });
+  });
+});
