07d0449fb2360beb78bc7d97c730245b1075f88a
billodell <billodell5@gmail.com>
2018-02-25 16:50:39 -0800

    test passing

--- lab-bill/lib/hash-table.js
+++ lab-bill/lib/hash-table.js
@@ -4,7 +4,7 @@ const SLL = require('./link');
 
 const HashTable = module.exports = function(size=16) {
   this.size = size;
-  this.memory = [...Array(this.size)].fill(new SLL());
+  this.memory = [...Array(this.size)];
 };
 
 HashTable.prototype.hashKey = function(key) {
@@ -16,15 +16,14 @@ HashTable.prototype.hashKey = function(key) {
 
 HashTable.prototype.set = function(key, value) {
   if (typeof key !== 'string' || key === null) throw new Error;
-  let bucket = this.hashKey(key);
-  console.log(bucket);
-  console.log(this.memory[bucket]);
-  this.memory[bucket].insertHead({'key': key, 'value': value});
+  if (this.memory[this.hashKey(key)] === undefined) this.memory[this.hashKey(key)] = new SLL;
+  this.memory[this.hashKey(key)].insertHead({'key': key, 'value': value});
   return null;
 };
 
 HashTable.prototype.get = function(key) {
   if (typeof key !== 'string' || key === null) throw new Error;
+  if (this.memory[this.hashKey(key)] === undefined) return null;
   let curr = this.memory[this.hashKey(key)].head;
   while (curr) {
     if(curr.value.key === key) return curr.value.value;
@@ -35,10 +34,11 @@ HashTable.prototype.get = function(key) {
 
 HashTable.prototype.remove = function(key) {
   if (typeof key !== 'string' || key === null) throw new Error;
+  if (this.memory[this.hashKey(key)] === undefined) return null;
   let curr = this.memory[this.hashKey(key)].head;
   let prev = null;
   console.log(curr);
-  if (curr.key === key) curr = curr.next;
+  if (curr.value.key === key) this.memory[this.hashKey(key)].head = curr.next;
   while (curr) {
     if(curr.key === key) prev.next = curr.next;
     prev = curr;
@@ -48,18 +48,27 @@ HashTable.prototype.remove = function(key) {
   return null;
 };
 
-let test = new HashTable;
-test.set('a', 'a');
-test.set('b', 'b');
-test.set('c', 'c');
-test.set('d', 'd');
-test.get('a');
-console.log(test.memory[0]);
-console.log(test.memory[1]);
-console.log(test.memory[2]);
-console.log(test.memory[3]);
-console.log(test.memory[4]);
-console.log(test.memory[5]);
-console.log(test.memory[6]);
-console.log(test.memory[7]);
-// console.log(test.memory[6]);  
+// let test = new HashTable;
+// console.log(test);
+// test.set('a', 'a');
+// test.set('b', 'b');
+// test.set('c', 'c');
+// test.set('d', 'd');
+// console.log(test);
+// console.log(test.memory[2]);
+// // test.get('a');
+// // console.log(test.memory[0]);
+// // console.log(test.memory[1]);
+// // console.log(test.memory[2]);
+// // console.log(test.memory[3]);
+// // console.log(test.memory[4]);
+// // console.log(test.memory[5]);
+// // console.log(test.memory[6]);
+// // console.log(test.memory[7]);
+// // console.log(test.memory[6]);  
+// let link = new SLL;
+// link.insertHead(1);
+// link.insertHead(2);
+// link.insertHead(3);
+// link.insertHead(4);
+// console.dir(link, {depth: null});
