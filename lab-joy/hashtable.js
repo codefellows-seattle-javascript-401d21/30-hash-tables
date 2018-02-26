5a608a9c6ad2784adc4d978bdcb69a8e85decee8
strawbee <yuejiao.hou@gmail.com>
2018-02-26 10:53:05 -0800

    fix

--- lab-joy/hashtable.js
+++ lab-joy/hashtable.js
@@ -37,7 +37,10 @@ HashTable.prototype.remove = function(key) {
   let bucket = this.buckets[this.hash(key)];
   if (!bucket) return null;
   let current = bucket.head;
-  if (!current.next) bucket = undefined;
+  if (!current.next) {
+    bucket = undefined;
+    return;
+  }
   while (current.next) {
     let prev = current;
     current = current.next;
