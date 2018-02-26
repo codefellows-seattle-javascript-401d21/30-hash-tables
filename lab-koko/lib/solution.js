146fbce5eb8e66048af64b789077a49cd7e5c03b
Koko Kassa <kowserk7@gmail.com>
2018-02-25 20:30:40 -0800

    added what I could

---
+++ lab-koko/lib/solution.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const HashTable = module.exports = function(size=1024) {
+  this.size = size;
+  this.memory = [...Array(this.size)];
+};
+HashTable.prototype.hashKey = function(key) {
+  let hash = key.split('').reduce((a, b) => a + b.charCodeAt(0), 0) % this.size;
+  return hash;
+};
+HashTable.prototype.set = function(key, value) {
+  this.memory[this.hashKey(key)] = value;
+};
+HashTable.prototype.get = function(key) {
+  return this.memory[this.hashKey(key)];
+};
+HashTable.prototype.remove = function() {
+};
