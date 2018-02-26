f2b645b87c72fbcfa48339b4abc24a8550166727
Victoria Oh <veoh1989@gmail.com>
2018-02-25 22:31:40 -0800

    hashkey tests done

--- lab-liza/__test__/soution.test.js
+++ lab-liza/__test__/soution.test.js
@@ -9,9 +9,32 @@ describe('HashTable', () => {
     it('should return with a new instance of the hashtable', () => {
       expect(new HashTable()).toBeInstanceOf(HashTable);
     });
+
     it('should have a default size of 1024', () => {
-      let hashTable = new HashTable();
-      expect(hashTable.size).toEqual(1024);
+      let hashTest = new HashTable();
+      expect(hashTest.size).toEqual(1024);
     });
+  });
+});
+
+describe('#hashKey', () => {
+  it('should return a numerical value', () => {
+    let hashTest = new HashTable(99);
+    let hash = hashTest.hashKey('foo');
+    expect(typeof hash === 'number').toBe(true);
+  });
+
+  it('should return a hash between 0-99', () => {
+    let hashTest = new HashTable(99);
+    let hash = hashTest.hashKey('foo');
+    expect(hash).toBeGreaterThanOrEqual(0);
+    expect(hash).toBeLessThan(99);
+  });
+
+  it('should return the value of the same hashkey', () => {
+    let hashTest = new HashTable(99);
+    let hash = hashTest.hashKey('foo');
+    let hash2 = hashTest.hashKey('foo');
+    expect(hash).toEqual(hash2);
   });
 });
