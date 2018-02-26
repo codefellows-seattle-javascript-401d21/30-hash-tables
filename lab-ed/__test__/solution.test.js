f69530c4288534330cfb1d83c94ada08e1ecd977
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-02-25 16:15:10 -0800

    scaffold

---
+++ lab-ed/__test__/solution.test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+const solution = require('../lib/solution')
+
+describe('Solution Module', function() {
+  describe('#', function() {
+    it('should return null if no arguments passed', function() {
+      expect(solution.solution()).toEqual(null)
+    })
+  })
+})
---
+++ lab-ed/lib/solution.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const solution = module.exports = {}
+
+solution.hash = function(key) {
+  // converts a string into a number that will index your buckets
+}
+
+solution.set = function(key, value) {
+  // stores a value in the hashed keys bucket
+}
+
+solution.get = function(key) {
+  // looks in the hashed keys bucket and returns the value of the node containing the key, or null if not found
+}
+
+solution.remove = function(key) {
+  // removes the dll node node containing the key
+}
