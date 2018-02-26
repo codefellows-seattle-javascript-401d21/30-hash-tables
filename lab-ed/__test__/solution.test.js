417d41687186754e68e8208c134b416c5f429a88
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-02-25 17:39:17 -0800

    add set

--- lab-ed/__test__/solution.test.js
+++ lab-ed/__test__/solution.test.js
@@ -4,8 +4,13 @@ const solution = require('../lib/solution')
 
 describe('Solution Module', function() {
   describe('#', function() {
-    it('should return null if no arguments passed', function() {
-      expect(solution.solution()).toEqual(null)
+    it('should return a hash integer used as array index', function() {
+      expect(solution.hash('abcde')).toEqual(30)
+    })
+    describe('#', function() {
+      it('should return a bucket with a sll at index 30', function() {
+        expect(solution.set('abcde', 'hello')).toEqual( {'buckets': [undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, undefined, {'key': 'abcde', 'next': null, 'value': 'hello'}]})
+      })
     })
   })
 })
