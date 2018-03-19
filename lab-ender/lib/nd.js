0fffb15e6e892dacaf7e47be645812b824d1f3a1
Ender Smith <Ender Smith>
2018-03-18 23:48:58 -0700

    pre-emptive turn in

---
+++ lab-ender/lib/nd.js
@@ -0,0 +1,12 @@
+'use strict';
+
+class Node {
+  constructor(val) {
+    if (!val) throw new Error('Not a valid value');
+
+    this.val = val;
+    this.next = null;
+  }
+}
+
+module.exports = Node;
